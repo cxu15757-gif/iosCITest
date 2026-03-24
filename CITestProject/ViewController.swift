f8f5939cdebb73f48a21986c70cde6cdc024ea65
cx <cxu15757@gmail.com>
2026-03-24 10:43:20 +0800

    init

---
+++ CITestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CITestProject
+//
+//  Created by ll on 2026/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
